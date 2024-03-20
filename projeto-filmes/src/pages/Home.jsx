9e57fe16d5f80b7ff53627402be0a816410c81be
Victor87dev <devsjoaovvictor@gmail.com>
2024-03-20 17:59:53 -0300

    feat: carregando filmes com .map

--- projeto-filmes/src/pages/Home.jsx
+++ projeto-filmes/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { useState, useEffect } from 'react'
 
+import MovieCard from '../components/MovieCard'
+
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
@@ -28,7 +30,13 @@ const Home = () => {
 
   return (
     
-    <div>{topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}</div>
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+      </div>
+    </div>
     
   )
 }
