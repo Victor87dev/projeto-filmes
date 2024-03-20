453ded73c14a620957ade0046012f5b59f5e9be6
Victor87dev <devsjoaovvictor@gmail.com>
2024-03-20 12:44:17 -0300

    feat: mais algumas configurações basicas para criar o projeto

--- projeto-filmes/src/App.jsx
+++ projeto-filmes/src/App.jsx
@@ -1,3 +1,6 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar"
+
 import './App.css'
 
 function App() {
@@ -5,7 +8,11 @@ function App() {
 
   return (
     <>
-      
+      <div className="App">
+        <Navbar />
+        <h2>Movies Lib</h2>
+        <Outlet />
+      </div>
     </>
   )
 }
