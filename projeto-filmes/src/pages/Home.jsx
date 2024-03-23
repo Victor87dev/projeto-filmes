1375714e72b1d5ab1284a730af7e82a5b1d90731
Victor87dev <devsjoaovvictor@gmail.com>
2024-03-23 13:02:37 -0300

    perf: importando css

--- projeto-filmes/src/pages/Home.jsx
+++ projeto-filmes/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react'
 
 import MovieCard from '../components/MovieCard'
 
+import "./MoviesGrid.css"
+
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
