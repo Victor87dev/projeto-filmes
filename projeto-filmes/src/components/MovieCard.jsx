a927d0b95fc7397903c1c31285fece19a51a17fb
Victor87dev <devsjoaovvictor@gmail.com>
2024-03-23 13:01:36 -0300

    feat: arrendondando nota do filme com o metodo toFixed()

--- projeto-filmes/src/components/MovieCard.jsx
+++ projeto-filmes/src/components/MovieCard.jsx
@@ -5,12 +5,15 @@ import { FaStar } from "react-icons/fa"
 const imageUrl = import.meta.env.VITE_IMG
 
 const MovieCard = ({ movie, showLink = true }) => {
+  
+  const notaFilme = movie.vote_average
+
   return (
     <div className="movie-card">
       <img src={imageUrl + movie.poster_path} alt={movie.title} />  
       <h2>{movie.title}</h2>
       <p>
-        <FaStar /> {movie.vote_average}
+        <FaStar /> {notaFilme.toFixed(1)}
       </p>
       {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
